e9752bf00d67d976a221fbeebc37bba2d1bb8271
Elias <Elias.Benfridja06@gmail.com>
2026-01-26 22:40:44 +0100

    Fix 12

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 ADD THIS LINE
+
   server: {
     host: true,
     allowedHosts: [
       '*.serveousercontent.com',
-      "244eec7fdb8f9546-154-121-90-220.serveousercontent.com"
+      '244eec7fdb8f9546-154-121-90-220.serveousercontent.com'
     ],
   },
 })
