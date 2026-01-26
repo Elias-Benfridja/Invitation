d4c6bf0b7c2a48eceece899196991fc1b058ce8a
Elias <Elias.Benfridja06@gmail.com>
2026-01-27 00:48:36 +0100

    fix video load

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import InfoPage from "./Pages/InfoPage"
-import EnvelopePage from "./Pages/EnvelopePage"
-import { Routes, Route } from "react-router-dom"
-function App() {
+import InfoPage from "./Pages/InfoPage";
+import EnvelopePage from "./Pages/EnvelopePage";
+import { Routes, Route } from "react-router-dom";
 
+function App() {
   return (
     <>
-    <video
+      {/* Hidden video to preload */}
+      <video
         src="/Videos/video6023884163138985617.mp4"
         preload="auto"
         style={{ display: 'none' }}
       />
 
+      {/* Your routes */}
       <Routes>
         <Route path="/" element={<EnvelopePage />} />
         <Route path="/info" element={<InfoPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
