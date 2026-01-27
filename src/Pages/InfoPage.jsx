912e333d19dc95fb1281a2cbaaa1d6da7ff5f8ce
Elias <Elias.Benfridja06@gmail.com>
2026-01-27 11:21:39 +0100

    add scroll

--- src/Pages/InfoPage.jsx
+++ src/Pages/InfoPage.jsx
@@ -5,13 +5,20 @@ import Icon from '@mui/material/Icon';
 import { motion } from 'framer-motion';
 import Header from '../Components/Header.jsx';
 import Location from '../Components/Location.jsx';
+import { useEffect, useRef } from 'react';
 
 function InfoPage() {
+  const videoRef = useRef(null);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            videoRef.current?.scrollIntoView({ behavior: "smooth" });
+        }, 9000);
+    }, []);
   return (
     <div>
       <Box sx={{width: '100vw', height: 'fit-content', background: "#0C2B4E", display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <Header />
-        <Location />
+        <Location ref={videoRef} />
       </Box>
     </div>
   );
