1d21254bd12f05da398290aa269d2e4ea50f4e33
Elias <Elias.Benfridja06@gmail.com>
2026-06-14 21:28:22 +0100

    changed3

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -103,7 +103,7 @@ function Header() {
               textAlign: "center",
               whiteSpace: "pre-line",
               fontFamily: fontToggle ? "Grenze, serif" : "Grenze, serif",
-              fontSize: fontToggle ? "30px" : "24px",
+              fontSize: fontToggle ? "26px" : "24px",
               mb: 2,
             }}
           >
