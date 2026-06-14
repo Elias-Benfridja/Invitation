a97c5039a6070cabf1b20bce183e571ef55991a2
Elias <Elias.Benfridja06@gmail.com>
2026-06-14 22:43:25 +0100

    changed4

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -127,7 +127,7 @@ function Header() {
               textAlign: "center",
               whiteSpace: "pre-line",
               fontFamily: fontToggle ? "Grenze, serif" : "Great Vibes, cursive",
-              fontSize: fontToggle ? "24px" : "24px",
+              fontSize: fontToggle ? "24px" : "36px",
             }}
           >
             {secondLine}
@@ -150,7 +150,7 @@ function Header() {
               textAlign: "center",
               whiteSpace: "pre-line",
               fontFamily: fontToggle ? "Grenze, serif" : "Great Vibes, cursive",
-              fontSize: fontToggle ? "24px" : "24px",
+              fontSize: fontToggle ? "24px" : "36px",
             }}
           >
             {thirdLine}
@@ -172,7 +172,7 @@ function Header() {
               textAlign: "center",
               whiteSpace: "pre-line",
               fontFamily: "Great Vibes, cursive",
-              fontSize: "24px",
+              fontSize: "36px",
             }}
           >
             {fourthLine}
